23d106844e81c29a44aa2008d93ce03b420ff3bb
jaj2003 <jaj2003@hw.ac.uk>
2023-11-09 20:39:28 +0400

    code for week4

--- week4/main.js
+++ week4/main.js
@@ -1,32 +1,8 @@
-var myCity = {
-    "name": "Dubai",
-    "country": "UAE",
-    "place": "Dubai Mall"
-};
-console.log(myCity.name); // Outputs: Dubai
-console.log(myCity.country); // Outputs: UAE
-var myCity = ["Dubai", "UAE", "Dubai Mall"];
-console.log(myCity[1].console); // Outputs: Dubai
-
-var myCities = [
-    {
-        "name": "Dubai",
-        "country": "UAE",
-        "place": "Dubai Mall"
-    },
-    {
-        "name": "Milan",
-        "country": "Italy",
-        "place": "Duomo Di Milano"
-    }
-];
-console.log(myCities[1].country); // Outputs: Italy
-
 var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://jaj2003.github.io/F28WP-lab-1/cities1.json');
+    ourRequest.open('GET', 'https://jaj2003.github.io/F28WP-lab-1/week4/cities1.json');
     ourRequest.onload = function () {
         var ourData = JSON.parse(ourRequest.responseText);
         renderHTML(ourData);
